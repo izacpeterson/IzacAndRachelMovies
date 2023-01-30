1b364c57d9e198babad5ba40b3d385346d84b7c9
izacpeterson <izacpeterson@gmail.com>
2023-01-30 15:15:50 -0700

    service worker fetch

--- src/service-worker.js
+++ src/service-worker.js
@@ -5,3 +5,4 @@ self.addEventListener("install", (event) => {
 self.addEventListener("activate", (event) => {
   console.log("Service worker activated");
 });
+self.addEventListener("fetch", (event) => {});
